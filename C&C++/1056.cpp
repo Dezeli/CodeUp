429a244a93537a4b9d6f4b96e36e147cd666b492
Dezeli <haterecursive@gmail.com>
2023-09-30 10:57:26 +0900

    Solve

---
+++ C&C++/1056.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a == b){
+        printf("0");
+    }
+    else {
+        printf("1");
+    }
+
+	return 0;
+}
+
